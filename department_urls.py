8275f3bf6a0417bced523f4a73c31552f66103dd
magebeam79 <waldenfam2@gmail.com>
2023-03-05 18:39:18 -0500

    added electronics section

--- department_urls.py
+++ department_urls.py
@@ -2,6 +2,7 @@ departments = [
     {'name': 'accessories', 'url': 'https://www.kohls.com/catalog/clearance-accessories.jsp?CN=Promotions:Clearance+Department:Accessories&kls_sbp=90595572255358814293312850185484413129&pfm=browse%20refine&PPP=48&S=7&sks=true'},
     {'name': 'beauty', 'url': 'https://www.kohls.com/catalog/beauty.jsp?CN=Department:Beauty&pfm=browse%20refine&kls_sbp=05834574949241830242529155580587980870&PPP=48&S=7&sks=true'},
     {'name': 'bed-bath', 'url': 'https://www.kohls.com/catalog/bed-bath.jsp?CN=Department:Bed%20%26%20Bath&pfm=browse%20refine&kls_sbp=05834574949241830242529155580587980870&PPP=48&S=7&sks=true'},
+    {'name': 'electronics', 'url': 'https://www.kohls.com/catalog/electronics.jsp?CN=Department:Electronics&pfm=browse&kls_sbp=05834574949241830242529155580587980870&PPP=48&S=7&sks=true'},
     {'name': 'health', 'url': 'https://www.kohls.com/catalog/clearance-health-personal-care.jsp?CN=Promotions:Clearance+Department:Health%20%26%20Personal%20Care&pfm=browse%20refine&kls_sbp=90595572255358814293312850185484413129&PPP=48&S=7&sks=true'},
     {'name': 'home', 'url': 'https://www.kohls.com/catalog/clearance-home-decor.jsp?CN=Promotions:Clearance+Department:Home%20Decor&kls_sbp=90595572255358814293312850185484413129&pfm=browse%20refine&PPP=48&S=7&sks=true'},
     {'name': 'kitchen', 'url': 'https://www.kohls.com/catalog/kitchen-dining.jsp?CN=Department:Kitchen%20%26%20Dining&CC=for_thehome-LN1.0-S-kitchendining&kls_sbp=05834574949241830242529155580587980870&PPP=48&S=7&sks=true'},
